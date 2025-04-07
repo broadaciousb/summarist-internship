35950587bd827f1215d205e5dcd5c98a26596640
Broady <broadybmx@gmail.com>
2025-04-07 18:37:05 -0400

    Commit to review how to make tailwind css override the global.css file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  important: true,
+
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
