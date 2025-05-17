c46415917bf6847f2b87bbb86a9cda6a88efae73
Broady <broadybmx@gmail.com>
2025-05-17 16:32:32 -0400

    Added console log for testing

--- components/RecommendedBooks.tsx
+++ components/RecommendedBooks.tsx
@@ -65,7 +65,9 @@ export default function RecommendedBooks({books}: any) {
         subscriptionRequired={book.subscriptionRequired}
     />
 })
+        
       }
+      {console.log(books)}
     </div>
   );
 }
